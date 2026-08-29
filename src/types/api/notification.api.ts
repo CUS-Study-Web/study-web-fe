0c4c2dd0192703432aacef55cac6d8d75466be3e
ducchinh <hoangducchinh730@gmail.com>
2026-08-29 17:07:47 +0700

    refactor(api): change NotificationType from enum to string type

--- src/types/api/notification.api.ts
+++ src/types/api/notification.api.ts
@@ -1,14 +1,4 @@
-export type NotificationType =
-  | 'ACCOUNT_UNLOCKED'
-  | 'ACCOUNT_LOCKED'
-  | 'ACCOUNT_BANNED'
-  | 'VIP_APPROVED'
-  | 'VIP_REJECTED'
-  | 'VIP_EXPIRING'
-  | 'COURSE_CONTENT_ADDED'
-  | 'NEW_DOCUMENT_CREATED'
-  | 'NEW_FLASHCARD_TOPIC'
-  | 'SYSTEM';
+export type NotificationType = string;
 
 export interface NotificationItem {
   id: string;
